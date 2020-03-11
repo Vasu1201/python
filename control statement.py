f5d2e6d20a4e3a347d63666bb4f914ef6c914bf0
Vasu1201 <60962600+Vasu1201@users.noreply.github.com>
2020-03-11 19:41:22 +0530

    Grade Alloaction Using Control Statements

---
+++ control statement.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter marks:"))
+
+if marks>=40 and marks<60:
+      print("Congratulation you got C Grade")
+
+if marks>=60 and marks<75:
+      print("congratulation you got B Grade")
+
+if marks>=75:
+      print("Congratulation you got A Grade")
+
+elif marks<40:
+      print("FAIL")
+      
