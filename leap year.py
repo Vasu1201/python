9dd767bc5c5b950e7e5ab63661e3cffd05cba4c9
Vasu1201 <60962600+Vasu1201@users.noreply.github.com>
2020-03-11 19:37:21 +0530

    Leap Year

---
+++ leap year.py
@@ -0,0 +1,17 @@
+y = int(input("enter any year"))
+
+if (y%100==0):
+      if (y%400==0):
+            print("leap year")
+
+
+      else:
+            print("not a leap year")
+
+else:
+      if(y%4==0):
+            print("leap year")
+
+      else:
+            pritn("not a leap year")
+            
