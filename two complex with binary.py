bb7060b162cbd70d4df350c28b5b1d4666c0f457
Vasu1201 <60962600+Vasu1201@users.noreply.github.com>
2020-03-13 19:34:38 +0530

    Two Complex With Binary
    Two Complex With Binary Using Operators in Python

---
+++ two complex with binary.py
@@ -0,0 +1,21 @@
+#python Program To perfrom Addition
+#of Two complex number using binary
+# + operator overloading
+
+class complex:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+
+    #adding two object
+    def __add__(self,other):
+        return(self.a + other.a , self.b + other.b)
+
+    def __str__(self):
+        return self.a,self.b
+
+ob1 = complex(1,2)
+ob2 = complex(2,3)
+ob3 = ob1 + ob2
+print(ob3)
