aaf537f378fa483c462abfa0fc434fca362aedac
Vasu1201 <60962600+Vasu1201@users.noreply.github.com>
2020-03-13 19:23:42 +0530

    Plus Operator
    Plus Operator Using Operator

---
+++ plus operator.py
@@ -0,0 +1,13 @@
+#python program to show use of
+# + operator for differnet purposes
+
+print(1+2)
+
+#concatenate two string
+print("FY" + "IT")
+
+#product two numbers
+print(3 * 4)
+
+#repeat the string
+print("HELLO" * 4)
